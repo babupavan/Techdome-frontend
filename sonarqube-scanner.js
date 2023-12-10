c4fa5a3aa6326b13829a8c16e4b417bd7fad92c0
PAVAN@BABU <122382391+gaddypavan@users.noreply.github.com>
2023-12-10 17:57:34 +0530

    Create sonarqube-scanner.js

---
+++ sonarqube-scanner.js
@@ -0,0 +1,20 @@
+const scanner = require('sonarqube-scanner');
+scanner(
+    {
+        serverUrl: "http://15.206.80.151:9000/",
+        options: {
+            "sonar.login": "admin",
+            "sonar.password": "babu",
+            "sonar.projectName": "react_app",
+            "sonar.projectDescription": "Just for demo...",
+            "sonar.sourceEncoding":"UTF-8",
+            "sonar.sources": "./src",
+            "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+            "sonar.exclusions": "**/*.test.tsx",
+            "sonar.tests":"./src",
+            "sonar.testExecutionReportPaths":"test-report.xml",
+            "sonar.javascript.lcov.reportPaths":"coverage/lcov.info"
+        },
+    },
+    () => process.exit()
+);
